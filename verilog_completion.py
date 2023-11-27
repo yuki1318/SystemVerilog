66d8029acc4df57a346ed29f7fbf95d32c0d7d40
Yuki Inui <yuuki.inui@jrce.co.jp>
2023-11-27 13:50:30 +0900

    補完の微調整

--- verilog_completion.py
+++ verilog_completion.py
@@ -1097,11 +1097,11 @@ class VerilogHelper():
             if always_label :
                 a_l +=  ' : proc_$1'
         a_l +=  '\n'
-        a_l += 'if(~'+rst_n_name + ') begin\n'
+        a_l += 'if (~'+rst_n_name + ') begin\n'
         a_l += '$1 <= 0;'
-        a_l += '\nend else '
+        a_l += '\nend\nelse '
         if clk_en_name != '':
-            a_l += 'if(' + clk_en_name + ') '
+            a_l += 'if (' + clk_en_name + ') '
         a_l+= 'begin\n'
         if not always_one_cursor:
             a_l += '$1 <= $2;'
